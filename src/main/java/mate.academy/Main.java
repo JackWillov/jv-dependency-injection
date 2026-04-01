e14efd278fb4ecda87667d4fe7dde02d8dde7283
Yulian Popovych <popovuch01@gmail.com>
2026-04-02 01:29:32 +0200

    add injector logic and annotation

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -3,6 +3,7 @@ package mate.academy;
 import java.util.List;
 import mate.academy.lib.Injector;
 import mate.academy.model.Product;
+import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductService;
 
 public class Main {
@@ -10,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
         // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
+        Object actual = injector.getInstance(FileReaderService.class);
+        injector.getInstance(ProductService.class);
         ProductService productService = null;
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,80 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private Map<Class<?>, Object> instancesMap = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
     }
 
+    //    && fields.getDeclaringClass().isAnnotationPresent(Component.class)
     public Object getInstance(Class<?> interfaceClazz) {
+        Object clazzImplementationInstance = null;
+        Class<?> clazz = findImplementation(interfaceClazz);
+        Field[] declaredField = clazz.getDeclaredFields();
+        for (Field field : declaredField) {
+            if (field.isAnnotationPresent(Inject.class)
+                    && field.getDeclaringClass().isAnnotationPresent(Component.class)) {
+                Object fieldInstance = getInstance(field.getType());
+
+                clazzImplementationInstance = createNewInstance(clazz);
+
+                try {
+                    field.setAccessible(true);
+                    field.set(clazzImplementationInstance, fieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Cant initialise field value"
+                            + clazz.getName() + field.getName());
+                }
+
+            }
+        }
+
+        if (clazzImplementationInstance == null) {
+            clazzImplementationInstance = createNewInstance(clazz);
+        }
+        return clazzImplementationInstance;
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClazz) {
+        Map<Class<?>, Class<?>> interfaceImplementation = new HashMap<>();
+        interfaceImplementation.put(FileReaderService.class, FileReaderServiceImpl.class);
+        interfaceImplementation.put(ProductService.class, ProductServiceImpl.class);
+        interfaceImplementation.put(ProductParser.class, ProductParserImpl.class);
+        if (interfaceClazz.isInterface()) {
+            return interfaceImplementation.get(interfaceClazz);
+        }
         return null;
     }
+
+    public Object createNewInstance(Class<?> clazz) {
+        if (instancesMap.containsKey(clazz)) {
+            return instancesMap.get(clazz);
+        }
+
+        Constructor<?> constructor = null;
+
+        try {
+            constructor = clazz.getConstructor();
+            Object instance = constructor.newInstance();
+            instancesMap.put(clazz, instance);
+            return instance;
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Cant create new instance" + clazz.getName());
+        }
+
+    }
+
 }
